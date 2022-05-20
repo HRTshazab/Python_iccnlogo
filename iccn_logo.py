09adcef8396a7443b75224fb6a89c3a34a50a877
Fuzail98 <102956978+Fuzail98@users.noreply.github.com>
2022-05-20 12:43:51 +0530

    Update iccn_logo.py

--- iccn_logo.py
+++ iccn_logo.py
@@ -7,7 +7,6 @@ sshcli.load_system_host_keys()
 sshcli.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 ipaddr = input('Enter the IP address of the host: ')
-# user = input('Enter the username: ')
 print('Logging in as root...')
 time.sleep(1)
 pswd = getpass.getpass('Enter the root password: ')
@@ -17,30 +16,31 @@ print(f'Connecting to {ipaddr}...')
 sshcli.connect(hostname=ipaddr, port=22, username='root', password=pswd, look_for_keys=False, allow_agent=False)
 time.sleep(2)
 
-# print('Invoking Shell!!!')
 shell = sshcli.invoke_shell()
-
 print('Connected!!!')
+time.sleep(1)
 print('\n')
 
-commands = ('8', 'git clone https://github.com/Fuzail98/iccn-theme.git',
-            'cd iccn-theme/', 'cp *.* /usr/local/opnsense/www/themes/tukan/build/images/', 'cd ..', 'rm -r iccn-theme/')
+with open('commands.txt') as f:
+    commands = f.read().splitlines()
 
 print(f'Changes taking place on {ipaddr}, Please wait...')
 for cmnd in commands:
-    # print(f'Sending Command: {cmnd}')
     shell.send(cmnd + '\n')
     time.sleep(2)
 
+out = shell.recv(10000)
+
+shell.send('ls /usr/local/opnsense/www/themes/tukan/build/images/' + '\n')
+time.sleep(2)
 output = shell.recv(10000)
-out = output.decode()
-# print(out)
+print('\n')
+print('#' * 100)
+if 'iccn' in output.decode():
+    print('Changes have been made! Please refresh your GUI and/or clear the cache on your browser.')
+else:
+    print("Changes haven't been made!!! Please try again.")
 
 if sshcli.get_transport().is_active():
-    # print('Closing Connection!!!')
-    # print('\n')
+    print('Closing connection...')
     sshcli.close()
-
-print('#' * 100)
-print('\n')
-print('Changes have been made! Please refresh your GUI and/or clear the cache on your browser.')
